28f6cb88494a21dcad4075f8759911464cd9497b
Pedro Leite <contato.pedro77@gmail.com>
2022-12-15 01:50:53 -0300

    feat: mobile hamburger button working

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".menu")
+const hamburgerButton = document.querySelector(".btn-mobile")
+const navLinks = document.querySelectorAll("#nav-link")
+const nav = document.querySelector('nav')
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("activate")
-  navMenu.classList.toggle("activate")
-  console.log("ola");
-})
+function toggleMenu(event) {
+  if(event.type === 'touchstart') event.preventDefault()
+  nav.classList.toggle('active')
+  console.log("oi");
+}
+
+function hideMenu() {
+  nav.classList.toggle('active')
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('click', hideMenu)
+});
+
+hamburgerButton.addEventListener('click', toggleMenu)
+hamburgerButton.addEventListener('touchstart', toggleMenu)
